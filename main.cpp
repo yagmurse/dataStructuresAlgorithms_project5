dbc3846bc9155adf76514eca55af3b17460b2e1a
Çağrı <gokceca@itu.edu.tr>
2019-12-11 12:25:07 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*
+Student Name:
+Student Number:
+Project Number: 5
+Compile Status: [SUCCESS/FAIL]
+Running Status: [SUCCESS/FAIL]
+Notes: Anything you want to say about your code that will be helpful in the grading process.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+
+    return 0;
+}
