3a26446d52c556356878dd308cfe3141806aa2ab
yagmurse <yselek14@gmail.com>
2019-12-19 16:11:29 +0300

    2017400273

--- main.cpp
+++ main.cpp
@@ -9,11 +9,13 @@ Notes: Anything you want to say about your code that will be helpful in the grad
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int main(int argc, char* argv[]){
-    int N;
-    int K;
+    freopen("input3.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    int N,K;
     cin >>N;
     cin >>K;
     vector<int> vec;
@@ -23,37 +25,27 @@ int main(int argc, char* argv[]){
         vec.push_back(in);
     }
     sort(vec.begin(),vec.end());
-
     int total=1;
-
-    vector<pair<int,int>> store;
+    vector<pair<long long int,long long int>> store;
+    pair<long long int,long long int> pairTmp=make_pair(0,0);
+    for(int i=0;i<K;i++) {
+        store.push_back(pairTmp);
+    }
+    long long int result=0;
     while(total<=N) {
-        long long int result=0;
-        if (total <= K) {
-            for (int i = 0; i < total; i++) {
-                result += vec[i];
-            }
-            cout << result << endl;
-            pair<int,int> temp=make_pair(result,result);
-            store.push_back(temp);
-            total++;
-            continue;
-        }
-        else {
-            for(int i=1;i<=K;i++) {
-                result+=vec[total-i];
-            }
-            int place=(total%K-1+K)%K;
-            long long int toAdd=store[place].second;
-            long long int prev=store[place].first;
-            store[place].first=prev+toAdd+result;
-            store[place].second=result+toAdd;
-            cout << store[place].first <<endl;
-            total++;
-            continue;
+        if(total>K)
+            result-=vec[total-K-1];
 
-        }
+        result+=vec[total-1];
 
+
+        int place=(total%K-1+K)%K;
+        long long int toAdd=store[place].second;
+        long long int prev=store[place].first;
+        store[place].first=prev+toAdd+result;
+        store[place].second=result+toAdd;
+        cout << store[place].first << " ";
+        total++;
 
     }
     return 0;
